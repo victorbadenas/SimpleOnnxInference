d34843887ec58b91920cbcdc46175a21f49ed996
vbadenas <victor.badenas@gmail.com>
2021-12-21 22:21:33 +0100

    update onnxruntime install script

--- scripts/installonnxruntime.sh
+++ scripts/installonnxruntime.sh
@@ -2,6 +2,6 @@
 cd /tmp
 wget https://github.com/microsoft/onnxruntime/releases/download/v1.10.0/onnxruntime-linux-x64-1.10.0.tgz
 tar -xvf onnxruntime-linux-x64-*.tgz
-mkdir -p ~/.local/include/
-mv onnxruntime-linux-x64-* ~/.local/include/onnxruntime
+mkdir -p ~/.local/include/onnxruntime
+mv onnxruntime-linux-x64-*/* ~/.local/include/onnxruntime
 cd -
