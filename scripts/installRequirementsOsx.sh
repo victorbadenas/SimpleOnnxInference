594d342bfe7de6a508df86e893ae0359e0b4892e
vbadenas <victor.badenas@gmail.com>
2022-01-26 18:13:41 +0100

    add macos installation requirements script

--- scripts/installRequirementsOsx.sh
+++ scripts/installRequirementsOsx.sh
@@ -10,10 +10,10 @@ function installOnnx {
     echo Installing onnx in $OnnxDir ...
 
     cd /tmp
-    wget https://github.com/microsoft/onnxruntime/releases/download/v1.10.0/onnxruntime-linux-x64-1.10.0.tgz
-    tar -xvf onnxruntime-linux-x64-*.tgz
+    wget https://github.com/microsoft/onnxruntime/releases/download/v1.10.0/onnxruntime-osx-x86_64-1.10.0.tgz
+    tar -xvf onnxruntime-linux-osx-*.tgz
     mkdir -p $OnnxDir
-    mv onnxruntime-linux-x64-*/* $OnnxDir
+    mv onnxruntime-linux-osx-*/* $OnnxDir
     cd -
 }
 
@@ -25,15 +25,13 @@ function installOpenCV {
     unzip 3.4.16.zip
     mkdir -p opencv-3.4.16/build
     cd opencv-3.4.16/build
-    cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_LIST=core,improc,imgcodecs,dnn ..
+    cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
     make -j8
     sudo make install
 }
 
 # install dependancies
-sudo apt install -y build-essential unzip
-sudo apt install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+# brew install cmake gcc
 
 # Install opencv
 [ -d $OpenCVDir ] && echo found OpenCV installation in ${OpenCVDir}, skipping... || installOpenCV
