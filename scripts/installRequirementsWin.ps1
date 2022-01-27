3d3524416e2be8f423d22d48b90f86fad7e5948f
vbadenas <victor.badenas@gmail.com>
2022-01-27 16:27:10 +0100

    debug windows CI

--- scripts/installRequirementsWin.ps1
+++ scripts/installRequirementsWin.ps1
@@ -7,7 +7,7 @@ Set-Location .\external\
 # OpenCV install
 Invoke-WebRequest https://github.com/opencv/opencv/archive/refs/tags/3.4.16.zip -OutFile 3.4.16.zip
 Expand-Archive -LiteralPath .\3.4.16.zip -DestinationPath .\
-Set-Location .\opencv-3.6.16\
+Set-Location .\opencv-3.4.16\
 New-Item -ItemType directory -Path .\build\
 Set-Location .\build\
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX="..\..\" -D BUILD_LIST=core,improc,imgcodecs,dnn ..
